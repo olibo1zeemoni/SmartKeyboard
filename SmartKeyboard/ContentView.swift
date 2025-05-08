022f13ec2e8fbd086e3f3c56150ccc928efe0f11
George Boahene <39881594+olibo1zeemoni@users.noreply.github.com>
2025-05-08 16:05:15 +0000

    reinitialize

--- SmartKeyboard/ContentView.swift
+++ SmartKeyboard/ContentView.swift
@@ -6,17 +6,23 @@
 //
 
 import SwiftUI
+import Inject
 
 struct ContentView: View {
+    @ObserveInjection var inject
     @State var text = ""
     var body: some View {
         VStack(alignment: .leading) {
+            HStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                Text("Smart Keyboard")
+            }
             TextField("", text: $text)
         }
         .padding()
+        .enableInjection()
     }
 }
 
